64af388419d292dc814075925f27fa71f6f82654
diepsteven13 <diepsteven13@gmail.com>
2024-09-08 23:05:06 -0700

    "added dashboard"

---
+++ frontend/src/app/social-media-app/layout.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import styles from '../styles/app.layout.css';
+
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+</link>
+
+export default function RootLayout({ children }) {
+  return (
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <aside id="sidebar">
+            <header className={"header"}>
+              <Image
+                src="/app_name1.png"
+                width={200}
+                height={200} />
+            </header>
+            <br></br>
+            <p>
+              &emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+              Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at
+              nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+              tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+            </p>
+          </aside>
+        </div>
+        <div className="flex-grow p-0 md:overflow-y-auto md:p-0">{children}</div>
+      </div>
+  );
+}
--- frontend/src/app/social-media-app/page.js
+++ frontend/src/app/social-media-app/page.js
@@ -1,46 +1,19 @@
-'use client';
-
-import styles from '../styles/Home.module.css';
-import { useState } from 'react';
+import Image from "next/image";
+import styles from '../styles/app.layout.css';
 
 export default function Projects() {
-	const [inputValue, setInputValue] = useState('');
-	const [result, setResult] = useState('');
-
-	const handleButtonClick = async () => {
-		if (inputValue) {
-			try {
-				const trimmedInput = inputValue.trim();
-				const response = await(fetch(`http://localhost:8080/exampledata/${trimmedInput}`));
-				const data = await response.text();
-				setResult(data);
-				console.log("Data successfully retrieved.");
-			} catch (error) {
-				setResult('Error while fetching data');
-				console.error(error);
-			}
-		}
-	};
-
-
-	return (
-		<div className={styles.container}>
-			<header className={styles.banner}></header>
-
-			<input
-			  type="text"
-			  value={inputValue}
-			  onChange={(e) => setInputValue(e.target.value)}
-			  placeholder="Enter some text"
-			  className="text-black" 
-			/>
-
-			<button onClick={handleButtonClick} className={styles.button}>
-			  Fetch Data
-			</button>
-
-			<p className={styles.resultLabel}>{result}</p>
-
-		</div>
-	);
+  return (
+	<><header className={"header"}>
+	  </header>
+	  <main className={"main-container"}>
+        	  <Image
+			  src="/vision_text.png"
+			  width={200}
+			  height={200} />
+			  <Image
+				  src="/logo.png"
+				  width={200}
+				  height={200} />
+		  </main></>
+  );
 }
