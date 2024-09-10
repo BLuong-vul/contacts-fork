2334542c4af92961cad2493268008b0fe48b037e
BLuong-vul <bryanluongrh@gmail.com>
2024-09-10 15:33:54 -0700

    Fixed missing alt text

--- frontend/src/app/social-media-app/page.js
+++ frontend/src/app/social-media-app/page.js
@@ -4,16 +4,20 @@ import styles from '../styles/app.layout.css';
 export default function Projects() {
   return (
 	<><header className={"header"}>
-	  </header>
-	  <main className={"main-container"}>
-        	  <Image
-			  src="/vision_text.png"
-			  width={200}
-			  height={200} />
-			  <Image
-				  src="/logo.png"
-				  width={200}
-				  height={200} />
-		  </main></>
+	</header>
+	<main className={"main-container"}>
+    <Image
+		src="/vision_text.png"
+		width={200}
+		height={200}
+		alt="Vision text"
+	/>
+	<Image
+		src="/logo.png"
+		width={200}
+		height={200} 
+		alt="Vision logo"
+	/>
+	</main></>
   );
 }
