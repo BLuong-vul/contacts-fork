f74cdf51b77765aa2483fcb78aecf72109db1577
PhiNguyen357 <148721117+PhiNguyen357@users.noreply.github.com>
2024-09-20 11:32:17 -0700

    Add files via upload

--- frontend/src/app/social-media-app/page.js
+++ frontend/src/app/social-media-app/page.js
@@ -1,26 +1,33 @@
-import Image from "next/image";
+import homepagestyles from './social-media-homepage.module.css'; // adjust the path as necessary
 import styles from '../styles/app.layout.css';
+import Image from "next/image";
 import Navbar from "../../components/Navbar";
 import Link from 'next/link';
 
 export default function Projects() {
-  return (
-	<><header className={"header"}>
-		<Navbar/>
-	</header>
-	<main className={"main-container"}>
-    <Image
-		src="/vision_text.png"
-		width={200}
-		height={200}
-		alt="Vision text"
-	/>
-	<Image
-		src="/logo.png"
-		width={200}
-		height={200} 
-		alt="Vision logo"
-	/>
-	</main></>
-  );
+	return (
+		<><header className={styles.header}><Navbar /></header>
+		<main className={styles.mainContainer}>
+			<div className={homepagestyles.homeContainer}>
+          		<nav className={homepagestyles.navContainer}>
+          			<Link href="/social-media-app" className={homepagestyles.linkBox}>
+            			Home
+          			</Link>
+                	<Link href="/social-media-app/friends-list" className={homepagestyles.linkBox}>
+            			Friends List
+          			</Link>
+          		</nav>
+			</div>
+			<div>
+        	<Image
+				src="/vision_text.png"
+				width={200}
+				height={200} />
+			<Image
+				src="/logo.png"
+				width={200}
+				height={200} />
+			</div>
+			</main></>
+	);
 }
