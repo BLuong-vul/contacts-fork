eecf7428c89ef296ae48bfe2e44ded148e5e5e6e
diepsteven13 <diepsteven13@gmail.com>
2024-09-19 18:23:30 -0700

    added login and signup options onto front page

--- frontend/src/app/social-media-app/page.js
+++ frontend/src/app/social-media-app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from '../styles/app.layout.css';
 import Navbar from "../../components/Navbar";
+import Link from 'next/link';
 
 export default function Projects() {
   return (
 	<><header className={"header"}>
+		<Navbar/>
 	</header>
 	<main className={"main-container"}>
     <Image
