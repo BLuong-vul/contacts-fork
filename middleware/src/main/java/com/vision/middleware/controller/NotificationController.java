f31d8f7face6a82fc37ad8511a0b329adb94854f
kyuho <kyuho>
2024-10-14 12:00:42 -0700

    fjfjfjfjfjfjfjfj

---
+++ middleware/src/main/java/com/vision/middleware/controller/NotificationController.java
@@ -0,0 +1,22 @@
+package com.vision.middleware.controller;
+
+import com.vision.middleware.dto.NotificationDTO;
+import com.vision.middleware.service.NotificationService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class NotificationController {
+
+    @Autowired
+    private final NotificationService notificationService;
+
+    @MessageMapping("/notifications")
+    @SendTo("/notifications")
+    public NotificationDTO sendNotification()
+}
--- middleware/src/main/java/com/vision/middleware/service/NotificationService.java
+++ middleware/src/main/java/com/vision/middleware/service/NotificationService.java
@@ -3,6 +3,7 @@ package com.vision.middleware.service;
 import com.vision.middleware.domain.ApplicationUser;
 import com.vision.middleware.domain.Notification;
 import com.vision.middleware.dto.NotificationDTO;
+import com.vision.middleware.exceptions.IdNotFoundException;
 import com.vision.middleware.repo.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,7 @@ public class NotificationService {
 
         // save to repository to persist. Flush is a blocking method which is desired here.
         // Notifications should be saved to the repository before we send a message, as the user will
-        // send their own message to acknowledge it through a websocket.
+        // send their own message to acknowledge it through a websocket. This must be done before that.
         notificationRepository.saveAndFlush(notification);
 
         long userId = notification.getAssociatedUser().getUserId();
@@ -38,6 +39,15 @@ public class NotificationService {
 
         // send notification to user.
         messagingTemplate.convertAndSendToUser(String.valueOf(userId), "/notifications", notificationDTO);
+    }
+
+    public void acknowledgeNotification(long notificationId) {
+        Notification notification = notificationRepository.findById(notificationId).orElseThrow(
+                () -> new IdNotFoundException("Notification id not found")
+        );
+
+        notification.setAcknowledged(true);
+        notificationRepository.save(notification);
     }
 
     public List<Notification> getUnreadNotifications(ApplicationUser user) {
