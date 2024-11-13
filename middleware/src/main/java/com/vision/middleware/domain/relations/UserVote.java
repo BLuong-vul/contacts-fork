2b6bbe99984dc46f00bac7b2ab8e62946571325b
kyuho <>
2024-11-12 17:27:03 -0800

    groundwork for user voting impl

---
+++ middleware/src/main/java/com/vision/middleware/domain/relations/UserVote.java
@@ -0,0 +1,39 @@
+package com.vision.middleware.domain.relations;
+
+import com.vision.middleware.domain.ApplicationUser;
+import com.vision.middleware.domain.Post;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "user_votes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "post_id"})
+})
+public class UserVote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vote_id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private ApplicationUser user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(name = "vote_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private VoteType voteType;
+
+    public enum VoteType {
+        LIKE,
+        DISLIKE
+    }
+}
